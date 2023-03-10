a8e2443b7421b2a60c24e75779288c05ba387e57
吴琳琳 <wulinlin@keyike.com>
2023-03-10 13:48:36 +0800

    新增分页处理中间件

---
+++ internal/middlewares/pagination.go
@@ -0,0 +1,15 @@
+//@Author: wulinlin
+//@Description: 全局分页拦截处理
+//@File:  pagination
+//@Version: 1.0.0
+//@Date: 2023/03/10 13:45
+
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+func PaginateMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+
+	}
+}
